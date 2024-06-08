80c919adafd2610095f20c4deae12caf20b11df6
tharunbudidha <134671151+BudidhaTharun@users.noreply.github.com>
2024-06-08 16:28:17 +0530

    Add files via upload

---
+++ week1/src/App.jsx
@@ -0,0 +1,12 @@
+import QuoteGenerator from "./components/QuoteGenerator"
+
+function App() {
+
+  return (
+    <div className="App">
+       <QuoteGenerator/>
+    </div>
+  )
+}
+
+export default App
